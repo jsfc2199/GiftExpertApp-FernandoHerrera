2d253a9e241b0363debc0c5438024608d117903a
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-01-16 15:26:59 -0500

    agregar una nueva categoria

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -6,10 +6,17 @@ export const GiftExpertApp = () => {
   //listado persistente usando useState
   //lo inicializamos con un valor, en este caso un arreglo
   const [categories, setCategories] = useState(["One Punch"]);
+
+  //Desestructuramos para romper la relacion y retornar un arreglo nuevo
+  const onAddCategory = () => {
+    setCategories(["Other category", ...categories]);
+  };
+
   return (
     <>
       <h1>Gift App</h1>
       <ol>
+        <button onClick={onAddCategory}>Add Category</button>
         {/* usamos una expresion de js en llaves */}
         {categories.map((category) => {
           return <li key={category}>{category}</li>;
