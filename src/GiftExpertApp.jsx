9dc067ef3f63dfeaa042efeda4b5b32b44c02e56
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-01-16 15:20:45 -0500

    Creando una lista de categorias

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 export const GiftExpertApp = () => {
   // const apiKey = "AvcdGeNzW40xzmpObLOlBeUE3mFU67k3";
+
+  //listado persistente usando useState
+  //lo inicializamos con un valor, en este caso un arreglo
+  const [categories, setCategories] = useState(["One Punch"]);
   return (
     <>
       <h1>Gift App</h1>
+      <ol>
+        {/* usamos una expresion de js en llaves */}
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
