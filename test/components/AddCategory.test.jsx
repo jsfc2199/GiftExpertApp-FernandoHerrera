937b8bc1d7cc003cd50a29f728b7d6be360525dd
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-01-17 17:18:06 -0500

    Pruebas en add category evaluando onNewCategory con una jest function

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -19,8 +19,8 @@ describe("Pruebas en add category", () => {
     //  screen.debug()
   });
 
-  test('should llamar a onNewCategory y subir el formulario y limpiar el input una vez se ejecute el formulario con un valor', () => {
-    const inputValue = "dragon"
+  test("should llamar a onNewCategory y subir el formulario y limpiar el input una vez se ejecute el formulario con un valor", () => {
+    const inputValue = "dragon";
     render(<AddCategory onNewCategory={() => {}} />);
 
     const input = screen.getByRole("textbox");
@@ -30,11 +30,52 @@ describe("Pruebas en add category", () => {
     fireEvent.input(input, { target: { value: inputValue } });
 
     //simulamos el submit formulario
-    fireEvent.submit(form)
+    fireEvent.submit(form);
 
     //esperamos que sea vacio porque al hacer submit se limpia el el input
-    expect(input.value).toBe('')
-    
-  })
-  
+    expect(input.value).toBe("");
+  });
+
+  //prueba para evaluar onNewCategory correctamente sin tener una funcion vacia
+  /*
+  Para ello usamos jest functions
+  */
+  test("should llamar a onNewCategory", () => {
+    const inputValue = "dragon";
+    const onNewCategory = jest.fn();
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+
+    //cambiamos el valor del input
+    fireEvent.input(input, { target: { value: inputValue } });
+
+    //simulamos el submit formulario
+    fireEvent.submit(form);
+
+    expect(input.value).toBe("");
+
+    //evaluamos que se haya ejecutado bien el onNewCategory
+    expect(onNewCategory).toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+  });
+
+  test("should no llamar a onNewCategory", () => {
+    const onNewCategory = jest.fn();
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    //!no ejecutamos el input
+
+    // const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+
+    //simulamos el submit formulario
+    fireEvent.submit(form);
+
+    //evaluamos que se haya ejecutado bien el onNewCategory
+    expect(onNewCategory).toHaveBeenCalledTimes(0);
+    expect(onNewCategory).not.toHaveBeenCalledWith();
+  });
 });
